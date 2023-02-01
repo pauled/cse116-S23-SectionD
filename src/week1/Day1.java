b2754c50f61da67272207a72fce3801e739a7d92
pauled <drpauled@gmail.com>
2023-02-01 11:30:23 -0500

    day 2 of class and pom file updated to use Java 19

---
+++ src/week1/Day1.java
@@ -0,0 +1,24 @@
+package week1;
+
+public class Day1 {
+    public static double mBT(double in){
+        double out=in*2;
+        return out;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Nurse!");
+        int num=4;
+        double num2;
+        num2=3.4;
+        num=8;
+        String str="I am string";
+        int num3=6/4;
+        double num4=6.0/4.0;
+        System.out.println(num4);
+        boolean bool=true;
+        System.out.println(bool);
+        double out=mBT(2.0);
+        out=mBT(3.0);
+    }
+}
