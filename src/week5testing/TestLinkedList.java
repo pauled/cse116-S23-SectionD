9d9a6b737045fe96d1a79ac6ed265d6ed6031eb5
pauled <drpauled@gmail.com>
2023-03-01 10:54:36 -0500

    week5-2 class code

---
+++ src/week5testing/TestLinkedList.java
@@ -0,0 +1,51 @@
+package week5testing;
+
+import org.junit.Test;
+import week4DS1.LinkedListNode;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+public class TestLinkedList {
+    public LinkedListNode<Integer> firstNSquares(int n){
+        if (n<=0){
+            return null;
+        } else {
+            return firstNSquaresHelper(n,null);
+        }
+    }
+    private LinkedListNode<Integer> firstNSquaresHelper(int n,
+                                                        LinkedListNode<Integer> list){
+        if (n==1){
+            return new LinkedListNode<>(1,list);
+        } else {
+            return firstNSquaresHelper(n-1,new LinkedListNode<>(n*n,list));
+        }
+    }
+    public <T> void compareLinkedLists(LinkedListNode<T> l1,
+                                       LinkedListNode<T> l2){
+        if (!((l1==null) && (l2==null))){
+            assertTrue("l1 ",l1!=null);
+            assertTrue("l2 ",l2!=null);
+            assertEquals("node values nnot equal",
+                    l1.getValue(),l2.getValue());
+            compareLinkedLists(l1.getNext(),l2.getNext());
+        }
+    }
+    @Test
+    public void testLinkedList(){
+        LinkedListNode<Integer> computed=firstNSquares(3);
+        LinkedListNode<Integer> expected=new LinkedListNode<>(9,null);
+        expected=new LinkedListNode<>(4,expected);
+        expected=new LinkedListNode<>(1,expected);
+        compareLinkedLists(expected,computed);
+    }
+    @Test
+    public void testLinkedListSizeDifference(){
+        LinkedListNode<Integer> computed=firstNSquares(4);
+        LinkedListNode<Integer> expected=new LinkedListNode<>(9,null);
+        expected=new LinkedListNode<>(4,expected);
+        expected=new LinkedListNode<>(1,expected);
+        compareLinkedLists(expected,computed);
+    }
+}
