b99db28405002fa55fcc69243929320832b6f850
pauled <drpauled@gmail.com>
2023-04-10 11:02:24 -0400

    week10-1

---
+++ src/week9classes3/CreditCard.java
@@ -0,0 +1,10 @@
+package week9classes3;
+
+public class CreditCard {
+    private double balance;
+    private int pin;
+    private double creditLimit;
+    private double interestRate;
+    private int paymentDate;
+
+}
